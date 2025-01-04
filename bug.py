e373b30399ed2ca85261e6e27d9be9afaa05add7
BugHunter-X <askzenai@gmail.com>
2025-01-04 19:52:04 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,8 @@
+def my_function(x):
+    if x == 0:
+        return 0  # Correct return statement
+    else:
+        return 1 / x
+
+result = my_function(0)  # This will now return 0 correctly
+print(result)
